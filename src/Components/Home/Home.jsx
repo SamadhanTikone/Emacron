b25be48994514331077a1a1c09b8d3220e5cf3f8
SamadhanTikone <samadhantikone003@gmail.com>
2025-01-03 00:06:03 +0530

    sectionA & gsap added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import styles from "./home.module.css"
 import Button from '@mui/material/Button';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import SectionA from "./SectionA";
 
 export default function Home() {
 
@@ -13,7 +14,7 @@ export default function Home() {
     
     <div className="w-[50%] mt-[15%]">
 
-      <h1 className={`text-[70px] ${styles.heading}  text-right uppercase pl-4 font-bold`}>Turning Your <span className="text-[#363062]">Vision</span> into <span className="text-[#363062]">Reality</span>.</h1>
+      <h1 className={`text-[70px] ${styles.heading}  text-right uppercase pl-4 font-bold`}> Turning Your <span className="text-[#363062]">Vision</span> into <span className="text-[#363062]">Reality</span>.</h1>
 
       <p className="text-left mt-6 text-[18px] px-16">We specialize in turning your ideas into successful, real-world solutions. Our team collaborates closely with you to understand your vision and bring it to life with cutting-edge technology and expertise. Share your project idea with us, and let’s make it a reality together!</p>
 
@@ -34,11 +35,8 @@ export default function Home() {
     </div>
      </div>
     
-     <div className="bg-green-500 h-screen relative">
-
-
-   
-     </div>
+    <SectionA/>
+     
   </>
   )
 }
---
+++ src/Components/Home/SectionA.jsx
@@ -0,0 +1,73 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+
+export default function SectionA() {
+
+    useGSAP(()=>{
+        gsap.from(".box",{
+            x:100,
+            opacity:0,
+            stagger:0.2,
+            scrollTrigger :{
+                trigger: ".box", // Element that triggers the animation
+                start: "center 70%", // Start when the element is 80% in the viewport
+                end: "center top", // End when the element is 30% in the viewport
+                scrub: 3, // Scrub the animation with scroll
+                markers: true,
+            }
+            
+        })
+    })
+  return (
+   <>
+   
+   <div className="bg-gray-400 h-screen">
+
+    <div>
+        <h2 className=" box text-4xl font-bold underline uppercase text-center text-black py-12 mb-8">Our <span className=" text-[#363062]">Achievements</span></h2>
+
+<div className="grid md:grid-cols-4 gap-12 w-full px-12 ">
+
+
+<div className="flex justify-center items-center box bg-white  h-40 rounded-xl shadow-lg shadow-[#363062]">
+    <img src="./project-manager.png" alt="" className="w-24 shadow-xl" />
+    <p className="flex flex-col ml-2 text-lg text-black font-semibold"><span className="text-5xl font-bold text-[#363062]">90+</span>Happy Clients</p>
+</div>
+<div className="flex justify-center items-center box bg-white  h-40 rounded-xl shadow-lg shadow-[#363062]">
+    <img src="./project.png" alt="" className="w-24 " />
+    <p className="flex flex-col ml-2 text-lg  text-black font-semibold"><span className="text-5xl font-bold text-[#363062]">50+</span>Projects</p>
+</div>
+<div className="flex justify-center items-center box bg-white  h-40 rounded-xl shadow-lg shadow-[#363062]">
+    <img src="./rating.png" alt="" className="w-24 drop-shadow-sm " />
+    <p className="flex flex-col ml-2 text-lg text-black font-semibold"><span className="text-5xl font-bold text-[#363062]">22+</span>Rating</p>
+</div>
+<div className="flex justify-center items-center box bg-white  h-40 rounded-xl shadow-lg shadow-[#363062] ">
+    <img src="./team.png" alt="" className="w-24 shadow-gray-800" />
+    <p className="flex flex-col ml-2 text-lg text-black font-semibold"><span className="text-5xl font-bold text-[#363062]">10+</span>Team Members</p>
+</div>
+
+
+</div>
+
+    </div>
+      
+
+<div>
+
+<div>
+<h2 className="box text-4xl font-bold underline uppercase text-center text-black py-12 mt-8">Our <span className=" text-[#363062]">Mission</span></h2>
+
+    <p className="text-4xl box bg-white py-8 text-center font-bold text-black"><span className="font-bold quote text-6xl font-mono text-orange-500 ">&quot;</span>   Transforming Indian Dreams into Digital Reality: <span className="font-bold text-[#363062] ">Your Vision, Our Mission</span> <span className="font-bold text-6xl font-mono text-orange-500 ">&quot;</span> </p>
+</div>
+
+
+</div>
+
+      </div>
+   
+   </>
+  )
+}
